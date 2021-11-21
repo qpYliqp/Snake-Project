059a0911a734279d4090eafd991fa3ccebf2a068
Yli <yliane.villot@hotmail.fr>
2021-11-21 14:54:49 +0100

    GameManager Change
    Change For Snake

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,6 +9,48 @@ public class GameManager : MonoBehaviour
     public bool startPlaying;
     public Scroller Beat;
 
+    public GameObject snakeObject;
+    private SnakeControler snake;
+
+    [SerializeField]
+    private float timerToTurn;
+    private float valueOfTimerToTurn;
+
+    void snake()
+    {
+        timerToTurn -= Time.deltaTime;
+
+        if (timerToTurn <= 0.0f)
+        {
+            if (Input.GetKey("left"))
+            {
+                snake.setDirection(directions.left);
+                timerToTurn = valueOfTimerToTurn;
+            }
+            else if (Input.GetKey("right"))
+            {
+                snake.setDirection(directions.right);
+                timerToTurn = valueOfTimerToTurn;
+            }
+            else if (Input.GetKey("down"))
+            {
+                snake.setDirection(directions.down);
+                timerToTurn = valueOfTimerToTurn;
+            }
+            else if (Input.GetKey("up"))
+            {
+                snake.setDirection(directions.up);
+                timerToTurn = valueOfTimerToTurn;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            snake.addPart();
+        }
+    
+    }
+
     public static GameManager instance;
     // Start is called before the first frame update
     void Start()
@@ -31,6 +73,9 @@ public class GameManager : MonoBehaviour
 
             }
         }
+
+
+        snake();
     }
 
 
